14a40a8ef9296a53139c1400cae32013f4a00df3
swapnillondheecera <swapnil.londhe24@gmail.com>
2022-07-27 12:35:21 +0530

    modified:   Data_fetch_python/scrapeBot.py 	modified:   apps/home/urls.py 	modified:   apps/templates/home/index.html

--- Data_fetch_python/scrapeBot.py
+++ Data_fetch_python/scrapeBot.py
@@ -1,9 +1,10 @@
 import urllib.request, json
 import pandas as pd
 
+api = "f370525e-0480-4336-9bf4-9cdaf5ed772f"
 
 class scrapeBot():
-    def sendReturn(self):
+    def getData(self):
         self.key_list = ('all-ads','all-analytics','all-javascript','all-cms','all-Web-Master','all-cdn','all-media','all-cdns','all-framework','all-payment','all-Web-Server','all-hosting')
         self.value_list = []
         self.dict_values = {}
@@ -15,13 +16,8 @@ class scrapeBot():
             self.dict_values[self.key_list[i]] = self.value_list[i]
         self.df = pd.DataFrame.from_dict(self.dict_values, orient='index').transpose()
         
-        return self.df
-    
-    def returnData(self):
-        return self.data2, self.data[0]['companyname']
+        return self.df,self.data2[0]['companyname']
 
     def __init__(self,title):
-        with urllib.request.urlopen("https://api.builtwith.com/daltv1/api.json?KEY=59dd064f-4d0c-4ab6-8f5a-a8c699cbf391&LOOKUP="+title) as url:
+        with urllib.request.urlopen("https://api.builtwith.com/daltv1/api.json?KEY="+api+"&LOOKUP="+title) as url:
             self.data2 = json.loads(url.read().decode())
-
-        
--- apps/home/urls.py
+++ apps/home/urls.py
@@ -5,11 +5,13 @@ Copyright (c) 2019 - present AppSeed.us
 
 from django.urls import path, re_path
 from apps.home import views
+from django.conf.urls import url, include
 
 urlpatterns = [
 
     # The home page
     path('', views.index, name='home'),
+    
 
     # Matches any html file
     re_path(r'^.*\.*', views.pages, name='pages'),
